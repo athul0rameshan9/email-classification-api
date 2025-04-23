a07371e51316f306e63a0b59038427c61e7e6666
athul <athulramesh98@gmail.com>
2025-04-23 14:19:21 +0530

    2nd commit

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import uvicorn
 
 # This file serves as the entry point for running the FastAPI application.
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("api:app", host="127.0.0.1", port=7860, reload=True)
